eee20705b312451dc296498127556b7bd9dd3cde
Danial Maktabi <danialmaktabi@gmail.com>
2023-01-15 14:00:47 +0100

    tuple

--- week 1 (Tuples)/Product Tuple.py
+++ week 1 (Tuples)/Product Tuple.py
@@ -8,3 +8,20 @@ The oldPrice of the product
 The newPrice of the product which is based on the discount
 Test your solution in the Main method'''
 
+class Product:
+    def __init__(self,name,price,discount) -> None:
+        self.name = name
+        self.price = int(price)
+        self.discount = int(discount)
+    
+    def GetDiscount(self):
+        old_price = self.price
+        new_price = self.price - (self.price * (self.discount / 100))
+        return (self.name, old_price, new_price)
+
+if __name__ == "__main__":
+    product = Product("Shoes", 100, 20)
+    name, old_price, new_price = product.GetDiscount()
+    print("Product:", name)
+    print("Old price:", old_price)
+    print("New price:", new_price)
